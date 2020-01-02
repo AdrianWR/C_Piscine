d229daf83202919cbf15f83952cfd63a04f6f1cc
AdrianWR <adrian.w.roque@gmail.com>
2020-01-01 21:04:22 -0300

    Update test with shell log

--- c_piscine_c_08/ex02/main.c
+++ c_piscine_c_08/ex02/main.c
@@ -6,7 +6,7 @@
 /*   By: aroque <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 21:35:41 by aroque            #+#    #+#             */
-/*   Updated: 2019/12/05 21:43:13 by aroque           ###   ########.fr       */
+/*   Updated: 2019/12/31 01:42:16 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char *argv[])
 {
 	(void) argc;
 	int k = atoi(argv[1]);
+        printf("Valor Absoluto: %d\n", ABS(k));
 
 	
 }
