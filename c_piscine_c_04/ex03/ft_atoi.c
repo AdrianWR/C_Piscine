4ae450a15f5311a5c4aba0309cce4ea9db6e720b
AdrianWR <adrian.w.roque@gmail.com>
2020-01-17 14:27:12 -0300

    Improve code and add comments

--- c_piscine_c_04/ex03/ft_atoi.c
+++ c_piscine_c_04/ex03/ft_atoi.c
@@ -6,71 +6,43 @@
 /*   By: aroque <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 10:25:38 by aroque            #+#    #+#             */
-/*   Updated: 2019/12/09 12:15:45 by aroque           ###   ########.fr       */
+/*   Updated: 2020/01/17 09:57:43 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ *	Check if a char is ASCII space, tabulation, carriage return
+ *  or another space character as defined in isspace(3).
+ */
 int	ft_isspace(char str)
 {
-	if (str == ' ' || str == '\t' || str == '\n')
-	{
+	if (str == 0x20 || (str >= 0x09 && str <= 0x0d))
 		return (1);
-	}
-	else if (str == '\v' || str == '\f' || str == '\r')
-	{
-		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
 
-int	atoi_signal(char *str)
-{
-	int signal;
-
-	signal = 0;
-	while (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-		{
-			signal++;
-		}
-		str++;
-	}
-	while (*str < '0' || *str > '9')
-	{
-		return (0);
-	}
-	signal = (signal % 2) * -1;
-	signal ? signal : signal++;
-	return (signal);
-}
-
+/*
+ * Converts a string of characters to integer. An arbitrary amount
+ * of white spaces can be added at string start, followed by any
+ * amount of '+' or '-' signals. An even number of '-' cancels each
+ * other. The conversion is then made while the following chars are
+ * numeric, stopping at any other char identified. If no numeric char
+ * os identified, the function returns 0 (zero).
+ */
 int	ft_atoi(char *str)
 {
 	int n;
-	int aux;
-	int n_signal;
 	int signal;
 
 	n = 0;
-	aux = -1;
+	signal = 1;
 	while (ft_isspace(*str))
-	{
 		str++;
-	}
-	signal = atoi_signal(str);
 	while (*str == '+' || *str == '-')
-	{
-		str++;
-	}
+		if (*str++ == '-')
+			signal *= -1;
 	while (*str >= '0' && *str <= '9')
-	{
-		n = n * 10 + (*str - '0');
-		str++;
-	}
-	n_signal = n * signal;
-	return (n_signal);
+		n = n * 10 + (*str++ - '0');
+	return (n * signal);
 }
